132962a49dd64516cf8a4f9a4366b77707bfb932
Luis Moran <luismoran2010@gmail.com>
2020-06-28 10:17:04 -0400

    added my audio files

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const letter = document.querySelector('.letterHolder');
 letter.addEventListener('click', e => {
     // console.log(e.target.innerText[0]);
     // console.log(e.target);
+    // let alphabet = e.target.innerText[0].toLowerCase();
+    let sing = new Audio(`sounds/${e.target.innerText[0].toLowerCase()}.m4a`);
     e.target.classList.add('blink'); 
+    sing.play();
     setTimeout(()=>{
         e.target.classList.remove('blink');
     }, 1000);
